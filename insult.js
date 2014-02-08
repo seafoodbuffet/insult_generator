77c04bd5127e3f9c3523ce3ae7495158eb15610f
Cheng Zhou <cheng@barelycode.com>
2014-02-07 21:24:02 -0600

    First set of pages

---
+++ insult.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var Insulter = {
+	first:  ['lazy', 'stupid', 'insecure', 'idiotic', 'slimy', 'slutty', 
+		'smelly', 'pompous', 'communist', 'dicknosed', 'pie-eating', 'racist', 
+		'elitist', 'white trash', 'drug-loving', 'butterfaced', 'tone deaf', 
+		'ugly', 'creepy'],
+	second: ['douche', 'ass', 'turd', 'rectum', 'butt', 'cock', 'shit', 'crotch', 
+		'bitch', 'prick', 'slut', 'taint', 'fuck', 'dick', 'boner', 'shart', 'nut', 'sphincter'],
+	third: ['pilot', 'canoe', 'captain', 'pirate', 'hammer', 'knob', 'box', 'jockey', 
+		'nazi', 'waffle', 'goblin', 'blossum',  'biscuit', 'clown', 'socket', 'monster', 
+		'hound', 'dragon', 'balloon'],
+	
+	insult: function() {
+		return _.sample(this.first) + ' ' + _.sample(this.second) + ' ' + _.sample(this.third);
+	},
+	
+	handler: function(elem) {
+		var insult = this.insult();
+		elem.textContent = insult;
+	}	
+};
