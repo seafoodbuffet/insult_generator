960d45f6a8cf6a9f3b23517c28462b259941308e
Cheng Zhou <zhouc@advisory.com>
2014-02-13 14:29:11 -0600

    added insult

--- insult.js
+++ insult.js
@@ -4,12 +4,12 @@ var Insulter = {
 	first:  ['lazy', 'stupid', 'insecure', 'idiotic', 'slimy', 'slutty', 
 		'smelly', 'pompous', 'communist', 'dicknosed', 'pie-eating', 'racist', 
 		'elitist', 'white trash', 'drug-loving', 'butterfaced', 'tone deaf', 
-		'ugly', 'creepy'],
+		'ugly', 'creepy', 'syphilitic' ],
 	second: ['douche', 'ass', 'turd', 'rectum', 'butt', 'cock', 'shit', 'crotch', 
 		'bitch', 'prick', 'slut', 'taint', 'fuck', 'dick', 'boner', 'shart', 'nut', 'sphincter'],
 	third: ['pilot', 'canoe', 'captain', 'pirate', 'hammer', 'knob', 'box', 'jockey', 
 		'nazi', 'waffle', 'goblin', 'blossum',  'biscuit', 'clown', 'socket', 'monster', 
-		'hound', 'dragon', 'balloon'],
+		'hound', 'dragon', 'balloon', 'tard'],
 	
 	insult: function() {
 		return _.sample(this.first) + ' ' + _.sample(this.second) + ' ' + _.sample(this.third);
